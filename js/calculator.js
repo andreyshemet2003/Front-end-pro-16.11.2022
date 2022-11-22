9fbd60f944db150777bd1cf2e73e6b36398a024b
Andrew <andreyshemet2003@gmail.com>
2022-11-22 17:22:47 +0200

    code

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,3 @@
-const expr= prompt("Скажите, какую операцию хотите выполнить")
-
-if (true){
-  const firstNumber = prompt('Enter first number', 0);
-  const secondNumber = prompt('Enter second number', 0);
-switch (expr) {
-  case "Вычитание":
-    const diff = firstNumber - secondNumber;
-    alert(`Разница между ${firstNumber} и ${secondNumber} является ${diff}`)
-    break;
-  case "Сложение":
-    const add = firstNumber + secondNumber;
-    alert(`Разница между ${firstNumber} и ${secondNumber} является ${add}`)
-    break;
-  case "Умножение":
-    const mult = firstNumber * secondNumber;
-    alert(`Разница между ${firstNumber} и ${secondNumber} является ${mult}`)
-    break;
-  case "Деление":
-    const div = firstNumber / secondNumber;
-    alert(`Разница между ${firstNumber} и ${secondNumber} является ${div}`)
-    break;
-}
-}
+const hour = prompt("Введите сколько часов нужно конвертировать", 5);
+const second = hour * 60 * 60;
+alert(`Количество часов - ${hour}; после перевода в секунды - ${second}`)
