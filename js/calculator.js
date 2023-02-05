2dcda1c8008b6c285d932e523038d5e32ce37529
Andrew <andreyshemet2003@gmail.com>
2023-02-05 23:40:18 +0200

    commit

--- js/calculator.js
+++ js/calculator.js
@@ -1,11 +1,104 @@
-var fruits = ["apple", "carrot", "pear", "celery"];
 
-// foreach
-fruits.forEach(function(item) {
-console.log(item);
-});
+// Task 16
+const array = [1, 2, 3, 4, 5, 6, 7];
 
-// for ... of
-for (var item of fruits) {
-console.log(item);
+function removeElement(array, item) {
+  if (array.find((el) => el === item)) {
+    array.forEach((el) => {
+      if (el === item) {
+        const indexEl = array.indexOf(el);
+        array.splice(indexEl, 1);
+      }
+    });
+  } else {
+    console.log(`Error. Item ${item} undefind`);
+  }
 }
+
+removeElement(array, 5);
+console.log(array);
+
+//Task 17
+
+let alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789',
+    key = '';
+for(let i = 0; i < 16; i++){
+    key += alphabet[Math.round(Math.random() * (alphabet.length - 1))];
+}
+console.log(key);
+
+// Task 18
+
+//part 1
+/*Дано масив з елементами різних типів.
+Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.*/
+const randomArray = [4, -6, "hello", 10, -265, "Alex", undefined, "30"];
+function avg(arr) {
+    let sum = 0, counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'number') {
+        sum += arr[i];
+        count++;
+      }
+    }
+    return counter === 0 ? 0 : sum / counter;
+  }
+
+  //part 2
+/* Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak.
+У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії,
+вказаної в змінній znak.Обидва числа і знак виходять від користувача.*/
+let x = (prompt('Enter first number: '));
+let znak = prompt('Enter operator: ');
+let y = (prompt('Enter second number: '));
+
+  function doMath(x, znak, y) {
+    switch(znak) {
+      case '+':
+        return x + y;
+      case '-':
+        return x - y;
+      case '*':
+        return x * y;
+      case '/':
+        return x / y;
+      case '%':
+        return x % y;
+      case '^':
+        return x ** y;
+      default:
+        return 'Invalid operator';
+    }
+  }
+  console.log(doMath(x, znak, y));
+ 
+  // Task 3
+/* Написати функцію заповнення даними користувача двомірного масиву.
+Довжину основного масиву і внутрішніх масивів задає користувач.
+Значення всіх елементів всіх масивів задає користувач.*/
+  function Matrix2(firstArr, secondArr) {
+    let mainArray = [];
+  
+    for (let i = 0; i < firstArr; i++) {
+      let subArray = [];
+      for (let j = 0; j < secondArr; j++) {
+        subArray.push(prompt(`Enter matrix size[${i}][${j}]`));
+      }
+      mainArray.push(subArray);
+    }
+  
+    return mainArray;
+  }
+  let mainArray = Matrix2(3, 5);
+  console.log(mainArray);
+
+  // Task 4
+/* Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
+'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
+Вихідний рядок та символи для видалення задає користувач. */
+  function func(str, chars) {
+    return str.split('').filter(char => !chars.includes(char)).join('');
+  }
+  console.log(func(" hello world", ['l', 'd']));
+
+  // Мне казалось я сойду с ума от количества логических операций в этом задании, спасите. Я вроде-бы понимаю , но всё равно не понимаю как правильно в голове запомнить действие "char => !chars.includes(char))"
