d15fbc406efb0818f0e82bcbaecd996e7c62902e
Andrew <andreyshemet2003@gmail.com>
2023-02-25 11:05:51 +0200

    fixs

--- js/calculator.js
+++ js/calculator.js
@@ -1,74 +1,106 @@
-class Human {
-  constructor(name, age) {
+class Student {
+  constructor(name, surname, yearOfBirth) {
     this.name = name;
-    this.age = age;
-  }
-  showHumansInfo() {
-    console.log(`Name: ${this.name} / Age: ${this.age}.`);
-  }
-}
-
-const andrew = new Human("Andrew", 19);
-andrew.showHumansInfo();
-
-const laura = new Human("Laura", 17);
-laura.showHumansInfo();
-
-const kate = new Human("Kate", 22);
-kate.showHumansInfo();
-
-const pablo = new Human("Pablo", 36);
-pablo.showHumansInfo();
-
-
-class Auto {
-  owner;
-  constructor(brand, model, yearOfRelease, licensePlate) {
-    this.brand = brand;
-    this.model = model;
-    this.yearOfRelease = yearOfRelease;
-    this.licensePlate = licensePlate;
+    this.surname = surname;
+    this.yearOfBirth = yearOfBirth;
+    this.marks = [];
+    this.attendance = new Array(25);
   }
 
-  showAutoInfo() {
-    console.log(` 
-    Auto -
-      Brand: ${this.brand} 
-      Model: ${this.model} 
-      Year of release: ${this.yearOfRelease} 
-      LicensePlate: ${this.licensePlate}.`);
+  getStudentsAge() {
+    return 2023 - this.yearOfBirth;
   }
 
-  addOwner(human) {
-    if (human.age < 18) {
-      console.log(`${human.name}: cannot be the owner!
-   Age: ${human.age} y.o.`);
-      return;
+  getAverageScore() {
+    const sum = this.marks.reduce(function (acc, mark) {
+      return (acc += mark);
+    }, 0);
+    return sum / this.marks.length;
+  }
+
+  getAverageAttendance() {
+    let attendClass = 0,
+      lessonNum = 0;
+    this.attendance.forEach((elem) => {
+      if (elem === true) {
+        attendClass++;
+      }
+      if (elem === true || elem === false) {
+        lessonNum++;
+      }
+    });
+    return attendClass / lessonNum;
+  }
+
+  present() {
+    for (let i = 0; i < this.attendance.length; i++) {
+      if (this.attendance[i] === undefined) {
+        this.attendance[i] = true;
+        break;
+      }
     }
-    this.owner = human;
   }
 
-  showOwner() {
-    console.log(`The owner of ${this.brand}: ${this.owner.name}.
-    Age: ${this.owner.age} y.o.`);
+  absent() {
+    for (let i = 0; i < this.attendance.length; i++) {
+      if (this.attendance[i] === undefined) {
+        this.attendance[i] = false;
+        break;
+      }
+    }
+  }
+
+  summary() {
+    const averageScore = this.getAverageScore();
+    const attendance = this.getAverageAttendance();
+    if (averageScore > 90 && attendance > 0.9) {
+      return "Молодець!";
+    } else if (averageScore > 90 || attendance > 0.9) {
+      return "Добре, але можна краще!";
+    } else if (averageScore < 90 && attendance < 0.9) {
+      return "Редиска!";
+    }
+  }
+
+  showStudentsInfo() {
+    console.log(`
+       Name: ${this.name} ${this.surname} 
+       Age: ${this.getStudentsAge()}
+       Average grade: ${this.getAverageScore()}
+       Attendance: ${this.getAverageAttendance()}
+       Summery: ${this.summary()}`);
   }
 }
 
-const nissan = new Auto("Nissan", "Leaf", 2010, "UA E4873HR");
-nissan.showAutoInfo();
-nissan.addOwner(andrew);
-nissan.showOwner();
+const student1 = new Student("Jane", "Johnson", 2005);
+student1.marks = [90, 96, 100, 88, 92];
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+console.log(student1);
+student1.showStudentsInfo();
 
-const toyota = new Auto("Toyota", "Prado", 1987, "UA AB2152AH");
-toyota.showAutoInfo();
-toyota.addOwner(laura);
+const student2 = new Student("Max", "Smith", 2004);
+student2.marks = [88, 85, 95, 95];
+student2.present();
+student2.present();
+student2.absent();
+student2.present();
+student2.absent();
+student2.present();
+console.log(student2);
+student2.showStudentsInfo();
 
-const bmw = new Auto("BMW", "X5", 2005, "UA 00007");
-bmw.showAutoInfo();
-bmw.addOwner(kate);
-bmw.showOwner();
-
-const mazda = new Auto("Mazda", "5", 2003, "UA AA6734ZE");
-mazda.showAutoInfo();
-mazda.addOwner(pablo);
-mazda.showOwner();
+const student3 = new Student("Paul", "Kempbel", 2003);
+student3.marks = [50, 75, 60];
+student3.present();
+student3.absent();
+student3.present();
+student3.absent();
+student3.present();
+student3.absent();
+console.log(student3);
+student3.showStudentsInfo();
