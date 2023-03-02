2956db0403db9acb9d1d419c50b047f779633cc3
Andrew <andreyshemet2003@gmail.com>
2023-03-02 13:10:03 +0200

    fix

--- js/calculator.js
+++ js/calculator.js
@@ -1,63 +1,52 @@
-class Hamburger {
-  constructor(size, pack) {
-      this.size = size;
-      this.pack = pack;
-      this.full = [];
+class SuperMath {
+  check(obj) {
+    const answer = confirm(
+      `Чи хочете ви виконати саме цю дію ${obj.X} ${obj.znak} ${obj.Y} ?`
+    );
+
+    let result = 0;
+    if (answer) {
+      if (obj.znak === "+") {
+        result = obj.X + obj.Y;
+      } else if (obj.znak === "-") {
+        result = obj.X - obj.Y;
+      } else if (obj.znak === "*") {
+        result = obj.X * obj.Y;
+      } else if (obj.znak === "/") {
+        result = obj.X / obj.Y;
+      } else if (obj.znak === "%") {
+        result = obj.X % obj.Y;
+      }
+      console.log(result);
+    } else {
+      this.input();
+    }
   }
-  static sizeSmall = {
-      price: 50,
-      calories: 20,
-  };
-  static sizeBig = {
-      price: 100,
-      calories: 40,
-  };
-  static packCheese = {
-      price: 10,
-      calories: 20,
-  };
-  static packSalad = {
-      price: 20,
-      calories: 5,
-  };
-  static packPotato = {
-      price: 15,
-      calories: 10,
-  };
-  static toppingFlavoring = {
-      price: 15,
-      calories: 0,
-  };
-  static toppingMayo = {
-      price: 20,
-      calories: 5,
-  };
-  addTopping(topping) {
-      this.full.push(topping)
-  }
-  calculatorPrice() {
-      const sizePrice = this.size.price;
-      const packPrice = this.pack.price;
-      const fullPrice = this.full.reduce((acc, current) => acc + current.price, 0);
-      return sizePrice + packPrice + fullPrice;
-  }
-  calculatorCalories() {
-  const sizeCalories = this.size.calories;
-  const packCalories = this.pack.calories;
-  const fullCalories = this.full.reduce((acc, current) => acc + current.calories, 0);
-  return sizeCalories + packCalories + fullCalories;
+
+  input() {
+    let result = 0;
+    const numOne = Number(prompt("Введіть перше число"));
+    const numTwo = Number(prompt("Введіть друге число"));
+
+    const mathSign = prompt(
+      "Введіть потрібний вам знак, наприклад: + , - , * , / , %"
+    );
+    if (mathSign === "+") {
+      result = numOne + numTwo;
+    } else if (mathSign === "-") {
+      result = numOne - numTwo;
+    } else if (mathSign === "*") {
+      result = numOne * numTwo;
+    } else if (mathSign === "/") {
+      result = numOne / numTwo;
+    } else if (mathSign === "%") {
+      result = numOne % numTwo;
+    } else {
+      console.log("Виникла помилка,введіть вірний знак");
+    }
+    console.log(result);
   }
 }
-
-// маленький гамбургер з начинкою з сиру
-const hamburger = new Hamburger(Hamburger.sizeSmall, Hamburger.packCheese);
-// добавка з майонезу
-hamburger.addTopping(Hamburger.toppingMayo);
-// запитаємо скільки там калорій
-console.log(`Calories: ${hamburger.calculatorCalories()}`);
-// скільки коштує
-console.log(`Price: ${hamburger.calculatorPrice()}`);
-// я тут передумав і вирішив додати ще приправу
-hamburger.addTopping(Hamburger.toppingFlavoring);
-// А скільки тепер коштує?
-console.log(`Price with sauce: ${hamburger.calculatorPrice()}`);
+let obj = { X: 12, Y: 3, znak: "/" };
+const p = new SuperMath();
+p.check(obj);
